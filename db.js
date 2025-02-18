74237952610a5cc2ead45e815ee37dca4b755acf
dajjal <turanarican1985@gmail.com>
2025-02-18 23:11:12 +0300

    m

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 
 async function connectDB() {
-  await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect(
+    "mongodb+srv://hcproject2025:DhqBAs91jjHvej5q@hcproject.sln8p.mongodb.net/?retryWrites=true&w=majority&appName=hcproject"
+  );
   console.log(`MongoDB connection estabilished`);
 }
 
